1052636710cdf981501915c5ab9a127dc643543b
Mücahit <mucahittgedik0@icloud.com>
2023-01-16 21:39:58 +0100

    main

--- react-calculator/src/App.js
+++ react-calculator/src/App.js
@@ -1,72 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
-import {useState} from "react";
-
+import {useState} from 'react'
 function App() {
-const [calculation, setCalculation]=useState('')
-    const [output, setOutput]=useState('')
-    const actions = ['/', '*', '+', '-', '.']
-    const updateCalculation=value=>{
+  const [output , setOutput] = useState('')
+  const operators = ["/" , "*" , "+","-" , "."]
+  const createDigits =()=>{
+    const digits = []
 
+    for(let i=1 ; i<10 ; i++){
+      digits.push(
+        <button onClick={()=>calculating(i.toString())} key={i}>{i}</button>
+      )
+    }
+    return digits
+  }
+
+  const calculating = (value)=>{
     if(
-        actions.includes(value) & calculation === '' ||
-        actions.includes(value) & actions.includes(calculation.slice(-1))
+      operators.includes(value) & output === '' ||
+      operators.includes(value) & operators.includes(output.slice(-1))
     ){
-        return
+      return;
     }
-    setCalculation(calculation+value)
+    setOutput(output+value)
+  }
 
-        if (!actions.includes(value)){
-            setOutput(eval(calculation+value).toString())
-        }
-
-    }
-   const createDigits =()=>{
-       const digits=[]
-
-       for (let i=1;i<10; i++){
-           digits.push(
-               <button onClick={()=>updateCalculation(i.toString())} key={i}>{i}</button>
-           )
-       }
-       return digits
-   }
-const calculate=()=>{
-    setCalculation(eval(calculation).toString())
-}
-    const clear=()=>{
-        if(calculation === ''){
-            return
-        }
-        const value=calculation.slice(0,-1)
-        setCalculation(value)
-    }
+  const calculated = ()=>{
+    setOutput(eval(output).toString())
+  }
 
   return (
-      <div>
-          <div className="calc-grid">
-      <div className='output'>
-          {calculation||'0'}
-          {output ? <span className="preRes">{output}</span>:''}
-      </div>
     <div>
-        <div className='ops'>
-          <button onClick={()=>{updateCalculation('/')}} >/</button>
-          <button onClick={()=>{updateCalculation('*')}}>*</button>
-          <button onClick={()=>{updateCalculation('+')}}>+</button>
-          <button onClick={()=>{updateCalculation('-')}}>-</button>
-          <button onClick={clear}> <img  width={40} height={40} src="https://cdn-icons-png.flaticon.com/512/159/159805.png" /></button>
-        </div>
-        <div className='dig'>
-            {createDigits()}
-        <button onClick={()=>{updateCalculation('.')}}>.</button>
-        <button onClick={()=>{updateCalculation('0')}}>0</button>
-        <button onClick={calculate}>=</button>
-            </div>
-          </div>  
-        </div>
+      <div className="output">
+        {output || '0'}
       </div>
+      <div className="operator">
+        <button onClick={()=> calculating('/')}>/</button>
+        <button onClick={()=> calculating('*')}>*</button>
+        <button onClick={()=> calculating('+')}>+</button>
+        <button onClick={()=> calculating('-')}>-</button>
+      </div>
+      <div className="numbers">
+        {createDigits()}
+        <button onClick={()=> calculating('.')}>.</button>
+        <button onClick={()=> calculating('0')}>0</button>
+        <button onClick={calculated}>=</button>
+      </div>
+    </div>
   );
 }
+
 
 export default App;
